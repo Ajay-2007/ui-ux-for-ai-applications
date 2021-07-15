5f98883f432f78a2ce8d22d8de69666ff86893e4
Ajay-2007 <iit2017040@iiita.ac.in>
2021-07-15 17:40:38 +0530

    star code for streamlit

---
+++ streamlit/app.py
@@ -0,0 +1,15 @@
+# Core Packages
+import streamlit as st
+
+
+# Additional Packages
+
+# Fxns
+
+def main():
+    """All your code goes here"""
+    st.title("Hello Streamlit Lovers")
+
+
+if __name__ == '__main__':
+    main()
